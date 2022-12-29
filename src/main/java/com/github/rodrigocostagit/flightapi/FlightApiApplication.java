88c7cab4e42880ae6daeb0e7ac6f861bb89579c4
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2022-12-29 18:27:14 +0000

    first commit

---
+++ src/main/java/com/github/rodrigocostagit/flightapi/FlightApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodrigocostagit.flightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/rodrigocostagit/flightapi/FlightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodrigocostagit.flightapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
